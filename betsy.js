c6ebcfb85bca9f65db3942ffdf4ee7519f54227e
Kerby Keith Aquino <skepfoosky15@gmail.com>
2023-09-08 19:32:34 +0800

    consolidate scripts

---
+++ betsy.js
@@ -0,0 +1,193 @@
+/**
+ * @param {any[]} i
+ * @returns
+ */
+const betsyDebug = (...i) =>
+  console.debug(`[BEtsy debug] ${i.length < 1 ? i.join(" ") : i.join("")}`)
+
+const $body = document.body
+/**
+ * @param {keyof HTMLElementTagNameMap | string} element
+ * @param {any} target
+ * @returns {HTMLElement}
+ */
+const _$ = (element, target = document) => target.querySelector(element)
+/**
+ * @param {keyof HTMLElementTagNameMap | string} element
+ * @param {any} target
+ * @returns {NodeListOf<Element>}
+ */
+const _$$ = (element, target = document) => target.querySelectorAll(element)
+
+/**
+ * Forces any input type to string
+ * @param {any} input
+ * @returns {string}
+ */
+const coerceToString = (input) => {
+  if (typeof input === "number") return input.toString()
+  if (typeof input === "object") return JSON.stringify(input)
+  return input
+}
+
+/**
+ * Removes the `target="_blank"` attribute from links that causes to open link to a new tab
+ */
+export const noNewTab = () => {
+  let overridesCount = 0
+
+  const linkMatches = [
+    `a[target*="etsy"]`,
+    `a[target="_blank"]:not([href^="/social"], [href^="https://play.google.com"], [href^="https://apps.apple.com"])`,
+  ]
+
+  const removeTargetAttributes = () => {
+    linkMatches.map((selector) => {
+      _$$(selector).forEach((element) => {
+        element.removeAttribute("target")
+
+        // For debug purposes and to keep track of links that are overriden
+        element.setAttribute("data-etsy-target-override", "")
+      })
+    })
+  }
+
+  const betsyLsKey = "_betsyData"
+  const betsyInit = {
+    init_date: new Date().toISOString(),
+    links_overriden: 0,
+  }
+
+  // Detect any DOM changes and re-execute the function when fetching listings through Etsy's API
+  const observer = new MutationObserver((mutationsList) => {
+    mutationsList.forEach(({ type }) => {
+      if (type === "childList") {
+        removeTargetAttributes()
+      }
+    })
+
+    const overridesDOM = _$$("[data-etsy-target-override]")
+    const overrideDOMLen = overridesDOM.length
+
+    if (overridesCount == overridesDOM.length) {
+      return
+    }
+
+    overridesCount = overrideDOMLen
+
+    const lsGetItem = localStorage.getItem(betsyLsKey)
+
+    if (!lsGetItem) {
+      localStorage.setItem(betsyLsKey, coerceToString(betsyInit))
+    }
+
+    if (lsGetItem) {
+      const { init_date, links_overriden: lo_temp } = JSON.parse(lsGetItem)
+      localStorage.setItem(
+        betsyLsKey,
+        coerceToString({
+          init_date,
+          links_overriden: lo_temp + overridesCount,
+        })
+      )
+    }
+
+    // prettier-ignore
+    const lsLinkOverriden = JSON.parse(lsGetItem || JSON.stringify(betsyInit)).links_overriden;
+
+    betsyDebug(`Overrided ${overridesCount} links, ${lsLinkOverriden} in total`)
+  })
+
+  observer.observe(document.body, { childList: true, subtree: true })
+
+  removeTargetAttributes()
+}
+
+/**
+ * Automatically expand listing details (i.e. expands the sections "Description", "Meet your sellers", etc.)
+ */
+export const expandListingDetails = () => {
+  const ListingInfoComponent = _$(".listing-info")
+
+  if (!!ListingInfoComponent) {
+    console.log(_$$("[data-appears-component-name]", ListingInfoComponent))
+  } else {
+    console.log("Listing element not found")
+  }
+}
+
+/**
+ * Makes the header "stick" to the window
+ */
+export const stickyHeader = () => {
+  const etsyHeader = _$("#gnav-header")
+  const domObserver = document.createElement("dom-observer")
+
+  // Fix horizontal scroll when user visits the Settings page
+  $body.style["overflow-x"] = "hidden"
+
+  const etsyHeaderStyleOverrides = {
+    position: "sticky",
+    top: "0",
+    transition: "box-shadow ease 300ms",
+  }
+
+  const observerStyles = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    height: "40px",
+  }
+
+  etsyHeader.setAttribute(
+    "style",
+    Object.entries(etsyHeaderStyleOverrides)
+      .map(([k, v]) => `${k}:${v} !important`)
+      .join("; ")
+  )
+
+  Object.entries(observerStyles).forEach(([sk, sv]) => {
+    domObserver.style[sk] = sv
+  })
+
+  const io = new IntersectionObserver(([entry]) => {
+    !entry.isIntersecting
+      ? (etsyHeader.style["box-shadow"] = "0 0 30px rgba(0, 0, 0, 0.5)")
+      : (etsyHeader.style["box-shadow"] = "0 0 30px rgba(0, 0, 0, 0.0)")
+  })
+
+  io.observe(domObserver)
+
+  $body.insertBefore(domObserver, etsyHeader)
+}
+
+/**
+ * Removes reference and click tracking by omitting ref and click queries
+ */
+export const removeTracking = () => {
+  const anchorTags = _$$("a")
+
+  const mutatedQuery = Array.from(anchorTags).map((links) => {
+    const fullPath = links.href.split("?").at(0)
+    const querySplit = links.search.split("&")
+
+    const queryFilterTracking = querySplit.filter(
+      (q) => !(q.includes("ref") || q.includes("click"))
+    )
+
+    const appendToFilteredPath =
+      queryFilterTracking.length == 0
+        ? fullPath
+        : `${fullPath}?${queryFilterTracking.join("&")}`
+
+    return {
+      target: links,
+      originalPath: links.href,
+      filteredPath: appendToFilteredPath,
+    }
+  })
+
+  mutatedQuery.forEach((link) => {
+    console.log(link)
+  })
+}
